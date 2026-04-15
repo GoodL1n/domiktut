43538bca8daa6a2d57d363af9e75979c8fe0ce95
SagidullinDV <dsagidullin@primegroup.msk.ru>
2026-04-15 13:22:33 +0300

    добавлен side-bar + стилизация под мобилку

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,27 @@ import "./preview.scss";
 import "./about.scss";
 import "./question.scss";
 import "./footer.scss";
+import "./side-menu.scss";
+
+const hamburger = document.querySelector(".header__hamburger-btn");
+const closeHamburgerBtn = document.querySelector(".side-menu__btn-close");
+const sideMenu = document.getElementById("side-menu");
+
+hamburger?.addEventListener("click", (element) => {
+  element.stopPropagation();
+
+  if (sideMenu) {
+    sideMenu.style.right = "0";
+
+    document.body.style.overflow = "hidden";
+  }
+});
+
+closeHamburgerBtn?.addEventListener("click", (element) => {
+  element.stopPropagation();
+
+  if (sideMenu) {
+    sideMenu.style.right = "-100%";
+    document.body.style.overflow = "";
+  }
+});
